a75a29d3f69cc7a558fe374324687e2664189eec
pierre <dommerc.pierre@gmail.com>
2020-11-04 23:41:20 +0100

    optimize battery module

--- src/battery.rs
+++ src/battery.rs
@@ -5,9 +5,11 @@
 use crate::error::Error;
 use crate::module::{BaruMod, RunPtr};
 use crate::pulse::Pulse;
-use crate::{read_and_parse, read_and_trim, Config as MainConfig, ModuleMsg};
+use crate::{Config as MainConfig, ModuleMsg};
 use serde::{Deserialize, Serialize};
 use std::convert::TryFrom;
+use std::fs::{self, File};
+use std::io::{self, prelude::*, BufReader};
 use std::sync::mpsc::Sender;
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -15,7 +17,16 @@ use std::time::Duration;
 
 const PLACEHOLDER: &str = "+@fn=1;󱃍+@fn=0;";
 const SYS_PATH: &str = "/sys/class/power_supply/";
-const NAME: &str = "BAT0";
+const BATTERY_NAME: &str = "BAT0";
+const UEVENT: &str = "uevent";
+const POWER_SUPPLY: &str = "POWER_SUPPLY";
+const CHARGE_PREFIX: &str = "CHARGE";
+const ENERGY_PREFIX: &str = "ENERGY";
+const FULL_ATTRIBUTE: &str = "FULL";
+const FULL_DESIGN_ATTRIBUTE: &str = "FULL_DESIGN";
+const NOW_ATTRIBUTE: &str = "NOW";
+const STATUS_ATTRIBUTE: &str = "POWER_SUPPLY_STATUS";
+
 const LOW_LEVEL: u32 = 10;
 const TICK_RATE: Duration = Duration::from_millis(500);
 
@@ -34,12 +45,17 @@ pub struct InternalConfig<'a> {
     low_level: u32,
     full_design: bool,
     tick: Duration,
+    uevent: String,
+    now_attribute: String,
+    full_attribute: String,
 }
 
-impl<'a> From<&'a MainConfig> for InternalConfig<'a> {
-    fn from(config: &'a MainConfig) -> Self {
+impl<'a> TryFrom<&'a MainConfig> for InternalConfig<'a> {
+    type Error = Error;
+
+    fn try_from(config: &'a MainConfig) -> Result<Self, Self::Error> {
         let mut low_level = LOW_LEVEL;
-        let mut name = NAME;
+        let mut name = BATTERY_NAME;
         let mut full_design = false;
         let mut tick = TICK_RATE;
         if let Some(c) = &config.battery {
@@ -58,12 +74,21 @@ impl<'a> From<&'a MainConfig> for InternalConfig<'a> {
                 tick = Duration::from_millis(t as u64)
             }
         }
-        InternalConfig {
+        let full_attr = match full_design {
+            true => FULL_DESIGN_ATTRIBUTE,
+            false => FULL_ATTRIBUTE,
+        };
+        let uevent = format!("{}{}/{}", SYS_PATH, &name, UEVENT);
+        let attribute_prefix = find_attribute_prefix(&uevent)?;
+        Ok(InternalConfig {
             name,
             low_level,
             full_design,
             tick,
-        }
+            uevent,
+            now_attribute: format!("{}_{}_{}", POWER_SUPPLY, attribute_prefix, NOW_ATTRIBUTE),
+            full_attribute: format!("{}_{}_{}", POWER_SUPPLY, attribute_prefix, full_attr),
+        })
     }
 }
 
@@ -108,16 +133,15 @@ pub fn run(
     _: Arc<Mutex<Pulse>>,
     tx: Sender<ModuleMsg>,
 ) -> Result<(), Error> {
-    let config = InternalConfig::from(&main_config);
+    let config = InternalConfig::try_from(&main_config)?;
     loop {
-        let energy_full = match config.full_design {
-            true => read_and_parse(&format!("{}{}/energy_full_design", SYS_PATH, config.name))?,
-            false => read_and_parse(&format!("{}{}/energy_full", SYS_PATH, config.name))?,
-        };
-        let energy_now = read_and_parse(&format!("{}{}/energy_now", SYS_PATH, config.name))?;
-        let status = read_and_trim(&format!("{}{}/status", SYS_PATH, config.name))?;
-        let capacity = energy_full as u64;
-        let energy = energy_now as u64;
+        let (energy, capacity, status) = parse_attributes(
+            &config.uevent,
+            &config.now_attribute,
+            &config.full_attribute,
+        )?;
+        let capacity = capacity as u64;
+        let energy = energy as u64;
         let battery_level = u32::try_from(100_u64 * energy / capacity)?;
         let mut color = &main_config.default_color;
         if status != "Charging" && battery_level <= config.low_level {
@@ -175,4 +199,89 @@ fn get_battery_icon<'a>(state: &'a str, level: u32) -> &'static str {
         },
         _ => "󱃍",
     }
+}
+
+fn parse_attributes(
+    uevent: &str,
+    now_attribute: &str,
+    full_attribute: &str,
+) -> Result<(i32, i32, String), Error> {
+    let file = File::open(&uevent)?;
+    let f = BufReader::new(file);
+    let mut now = None;
+    let mut full = None;
+    let mut status = None;
+    for line in f.lines() {
+        if now.is_none() {
+            now = parse_attribute(&line, &now_attribute);
+        }
+        if full.is_none() {
+            full = parse_attribute(&line, &full_attribute);
+        }
+        if status.is_none() {
+            status = parse_status(&line);
+        }
+    }
+    if now.is_none() || full.is_none() || status.is_none() {
+        return Err(Error::new(format!(
+            "unable to parse the required attributes in {}",
+            uevent
+        )));
+    }
+    Ok((now.unwrap(), full.unwrap(), status.unwrap()))
+}
+
+fn parse_attribute(line: &io::Result<String>, attribute: &str) -> Option<i32> {
+    if let Ok(l) = line {
+        if l.starts_with(&attribute) {
+            let s = l.split('=').nth(1);
+            if let Some(v) = s {
+                return v.parse::<i32>().ok();
+            }
+        }
+    }
+    None
+}
+
+fn parse_status(line: &io::Result<String>) -> Option<String> {
+    if let Ok(l) = line {
+        if l.starts_with(&STATUS_ATTRIBUTE) {
+            return l.split('=').nth(1).map(|s| s.to_string());
+        }
+    }
+    None
+}
+
+fn find_attribute_prefix<'a, 'b>(path: &'a str) -> Result<&'b str, Error> {
+    let content = fs::read_to_string(path)?;
+    let mut unit = None;
+    if content.contains(&format!(
+        "{}_{}_{}=",
+        POWER_SUPPLY, ENERGY_PREFIX, FULL_DESIGN_ATTRIBUTE
+    )) && content.contains(&format!(
+        "{}_{}_{}=",
+        POWER_SUPPLY, ENERGY_PREFIX, FULL_ATTRIBUTE
+    )) && content.contains(&format!(
+        "{}_{}_{}=",
+        POWER_SUPPLY, ENERGY_PREFIX, NOW_ATTRIBUTE
+    )) {
+        unit = Some(ENERGY_PREFIX);
+    } else if content.contains(&format!(
+        "{}_{}_{}=",
+        POWER_SUPPLY, CHARGE_PREFIX, FULL_DESIGN_ATTRIBUTE
+    )) && content.contains(&format!(
+        "{}_{}_{}=",
+        POWER_SUPPLY, CHARGE_PREFIX, FULL_ATTRIBUTE
+    )) && content.contains(&format!(
+        "{}_{}_{}=",
+        POWER_SUPPLY, CHARGE_PREFIX, NOW_ATTRIBUTE
+    )) {
+        unit = Some(CHARGE_PREFIX);
+    }
+    unit.ok_or_else(|| {
+        Error::new(format!(
+            "unable to find the required attributes in {}",
+            path
+        ))
+    })
 }
